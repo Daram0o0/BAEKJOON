87d2cb507d61b9f5711262c727334d7895ea1e16
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-01-07 20:08:19 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -3,7 +3,7 @@ function solution(a, b) {
     if(a%2 && b%2){
         return a**2 + b**2;
     }
-    else if((a%2 && !(b%2)) || (!(a%2) && b%2)){
+    else if(a%2 || b%2){
         return 2*(a + b);
     }
     else{
