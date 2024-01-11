5c820e1963583010d603c3c1783b7df315d52f39
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-01-11 16:47:57 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,15 @@
+function solution(before, after) {
+    var answer = 0;
+    var arrB = Array(26).fill(0);
+    var arrA = Array(26).fill(0);
+    
+    for(let i of before){
+        arrB[i.charCodeAt() - 97]++;
+    }
+
+    for(let i of after){
+        arrA[i.charCodeAt() - 97]++;
+    }
+
+    return arrB.join("") === arrA.join("") ? 1 : 0;
+}
