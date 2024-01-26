1764bb2949d1a014e8d2a266926aad7e066fdd62
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-01-26 23:00:22 +0900

    [level 1] Title: 추억 점수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -4,7 +4,7 @@ function solution(name, yearning, photo) {
     photo.forEach((names, i) => {
         let sum = 0;
         names.forEach((val, idx) => {
-            sum += yearning[name.indexOf(val)] ? yearning[name.indexOf(val)] : 0
+            sum += yearning[name.indexOf(val)] ?? 0
         })
         answer.push(sum)
     })
