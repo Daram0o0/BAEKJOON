7c1e71eaf2307dadba1ccfc79299604108cd469e
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-02-24 01:16:05 +0900

    [Bronze IV] Title: R2, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.js
@@ -0,0 +1,5 @@
+let fs = require("fs");
+let filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split(" ").map(Number);
+
+console.log(input[1] * 2 - input[0])
