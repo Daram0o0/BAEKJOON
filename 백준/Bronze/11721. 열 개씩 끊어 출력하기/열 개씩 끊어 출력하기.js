926e7ac951ad847657eb8b24f485bbaa0ef386b0
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-02-26 01:20:52 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 128 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(filePath).toString();
+
+for (let i = 0; i < input.length; i += 10) {
+  console.log(input.slice(i, i + 10))
+}
