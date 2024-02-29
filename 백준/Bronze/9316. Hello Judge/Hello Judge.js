36eea6a693d892bf7bd17b2df025470646f84a99
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-03-01 00:37:47 +0900

    [Bronze IV] Title: Hello Judge, Time: 128 ms, Memory: 9556 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs").readFileSync(filePath).toString();
+
+for (let i = 1; i <= +input; i++) {
+  console.log(`Hello World, Judge ${i}!`)
+}
