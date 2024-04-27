68e927778e7a4a3ab87c52288452bd16114b7be5
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-04-27 23:39:05 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,5 @@
 function solution(a, b) {
-    var answer = 0;
-    for(let i = 0; i<a.length; i++){
-        answer += a[i]*b[i]
-    }
-    return answer;
+    return a.reduce((acc,val,idx)=>{
+        return acc + val * b[idx]
+    },0)
 }
