a8020b4afca1488616991ccac1af380cb9c7cae3
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-05-04 01:31:18 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,13 +1,14 @@
 function solution(n, m) {
-//     최대공약수 : 둘 다 나눠지는 수
-//     최소공배수 : 공약수들의 곱..?(두 수 곱해서 최대공약수로 나누나..?)
-    let max = 1;
-    let min = 1;
-    for(let i = 1; i<=n; i++){
-        if(n%i===0 && m%i===0) max = i;
-    }
-    
-    min = n * m / max
-    
+    let max = gcd(n,m)
+let min = n * m /max
     return [max,min];
+}
+
+function gcd(a, b) {
+    // 만약 b가 0이면, a를 반환
+    if (b === 0) {
+        return a;
+    }
+    // 그렇지 않으면, a를 b로 나눈 나머지를 구하고 이를 새로운 b로 대체하고, 이전의 b를 새로운 a로 대체하여 재귀 호출
+    return gcd(b, a % b);
 }
