3869a173456a46e1531c1117d486d281b10b0334
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-05-29 02:22:48 +0900

    [Bronze V] Title: Can you add this?, Time: 136 ms, Memory: 9872 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.js
@@ -0,0 +1,6 @@
+const [n, ...arr] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const num = +n
+for(let i = 0; i<n; i++){
+    const [a,b]=arr[i].split(" ").map(Number)
+    console.log(a+b)
+}
