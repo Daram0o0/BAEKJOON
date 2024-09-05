35cfa806fea3dc6d71ca6f73f9db9a92dda34026
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-09-05 23:30:14 +0900

    [Bronze V] Title: HI-ARC=?, Time: 96 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let [H, I, A, R, C] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+console.log(H * I - A * R * C);
