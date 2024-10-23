ce3cf9ee2762ede061d85e0518b1e16cd3a419d6
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-10-23 22:58:56 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.py
@@ -0,0 +1,24 @@
+rank = int(input())
+
+if rank == 1:
+  print(12,1600)
+elif rank == 2:
+  print(11,894)
+elif rank == 3:
+  print(11,1327)
+elif rank == 4:
+  print(10,1311)
+elif rank == 5:
+  print(9,1004)
+elif rank == 6:
+  print(9,1178)
+elif rank == 7:
+  print(9,1357)
+elif rank == 8:
+  print(8,837)
+elif rank == 9:
+  print(7,1055)
+elif rank == 10:
+  print(6,556)
+elif rank == 11:
+  print(6,773)
