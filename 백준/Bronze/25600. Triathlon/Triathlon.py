04bebc36f315c71612978444c4ab089e8a3db867
Jiho <89921213+Daram0o0@users.noreply.github.com>
2024-12-05 01:46:21 +0900

    [Bronze IV] Title: Triathlon, Time: 60 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/25600. Triathlon/Triathlon.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+max = 0
+
+for _ in range(n):
+    a,d,g = map(int,input().split())
+    if a == d+g:
+        score = 2*a*(d+g)
+    else:
+        score = a*(d+g)
+    
+    if max < score:
+        max = score
+        
+
+print(max)
